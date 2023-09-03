bce1858a8139470eb6ea04085ac5c05d13b708d7
yoshinobufuruya <yoshinobu.furuya1988@gmail.com>
2023-09-03 14:01:03 +0900

    #3.7

--- myproject/lib/main.dart
+++ myproject/lib/main.dart
@@ -118,6 +118,7 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   color: const Color(0xFF1F2123),
                   borderRadius: BorderRadius.circular(20),
@@ -127,6 +128,7 @@ class App extends StatelessWidget {
                     30,
                   ),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -163,6 +165,20 @@ class App extends StatelessWidget {
                             ],
                           ),
                         ],
+                      ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(
+                            8,
+                            15,
+                          ),
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 88,
+                          ),
+                        ),
                       ),
                     ],
                   ),
