14648ce9426a03b439c2f3826555834de95c5339
yoshinobufuruya <yoshinobu.furuya1988@gmail.com>
2023-09-05 10:27:39 +0900

    #6.1

---
+++ webtoon/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Todays toon",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+      ),
+    );
+  }
+}
