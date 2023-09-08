e4b5ef60018302333d8afc3f4025e37d3adeb2de
yoshinobufuruya <yoshinobu.furuya1988@gmail.com>
2023-09-08 15:57:11 +0900

    #6.7

--- webtoon/lib/screens/home_screen.dart
+++ webtoon/lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:webtoon/models/webtoon_model.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +27,24 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text("there is data1");
+            return (ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                print(index);
+                return Text(
+                  webtoon.title,
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+            ));
           }
-          return const Text("No data");
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
